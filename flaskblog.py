11261d32a0a8c6dd9edef1705e89a2e64d01e00c
unknown <haziqkhanapiah@gmail.com>
2019-12-18 12:10:44 +0800

    first commit

---
+++ flaskblog.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Haziq',
+        'title': 'First Title',
+        'content': 'First content',
+        'date_posted': '1st January 2019'
+    },
+    {
+        'author': 'Afiq',
+        'title': 'Second Title',
+        'content': 'Second content',
+        'date_posted': '1st February 2019'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# can run like a normal python file
+if __name__ == '__main__':
+    app.run(debug=True)
